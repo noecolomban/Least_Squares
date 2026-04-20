06301fe4d7157cd225b57129992fb69a063536c9
Noé Colomban <noe.colomban@gmail.com>
2026-04-20 18:25:09 +0200

    Z transform

---
+++ src/asymptotics.py
@@ -0,0 +1,123 @@
+from xml.dom.minidom import Element
+from src.risk_computations import RiskComputations
+from src.least_squares import PowerLawRegression
+from src.SGD import SGD
+import numpy as np
+from scheduled import WSDSchedule, ConstantSchedule
+from src.visualization import Visualization
+import copy
+
+class ZTransform_constant:
+    def __init__(self, model: PowerLawRegression, x0, T=1000):
+        self.model = model
+        self.T = T
+        self.x0 = x0
+        self.m0 = self._compute_m0()
+        self.schedule = ConstantSchedule(steps=T, base_lr=0.1)
+        self.sgd = SGD(model, x0, self.schedule)
+        self.computations = RiskComputations(model, x0, [self.schedule], ["constant"], sgd_class=SGD)
+
+        self.computations.optimize_all_base_lrs(change_eta=True)
+
+        self._z_transform_results = {}
+        self._a = {}
+
+    def _compute_m0(self):
+        """Compute m0 = diag(Q^T * (x0 - x*) * (x0 - x*)^T * Q)"""
+        Sigma0 = np.outer(self.x0.flatten() - self.model.x_star.flatten(), self.x0.flatten() - self.model.x_star.flatten())
+        m0, _ = self.model.compute_M_t(Sigma0)
+        return m0
+    
+    def a(self, i):
+        assert 0 <= i < self.model.dim, "i must be between 0 and dim-1"
+        if i in self._a:
+            return self._a[i]
+        else:
+            eta = self.schedule.get_base_lr()
+            Lambda_vals = self.model.Lambda_vals
+            a = (1 - eta*Lambda_vals[i])**2 + 2*eta**2*Lambda_vals[i]**2
+            self._a[i] = a
+            return a
+
+
+    def compute_z_transform_result(self, i=None):
+        """Compute the Z-transform result at step T using m0 and the eigenvalues."""
+        if i is None:
+            i = self.model.dim - 1
+        assert 0 <= i < self.model.dim, "i must be between 0 and dim-1"
+        
+        if i in self._z_transform_results:
+            return self._z_transform_results[i]
+
+        Lambda_vals = self.model.Lambda_vals
+        eta = self.schedule.get_base_lr()  
+        sigma_sq = self.model.sigma**2
+
+        if 0 not in self._z_transform_results:
+            self._z_transform_results[0] = 0.5 * (eta**2 * Lambda_vals[0]**2 * sigma_sq) / (1 - self.a(0))
+
+        current_max_t = max(self._z_transform_results.keys())
+        
+        for j in range(current_max_t + 1, i + 1):
+            prev_val = self._z_transform_results[j - 1] 
+            current_term = 0.5 * (eta**2 * Lambda_vals[j]**2 * sigma_sq) / (1 - self.a(j))
+            self._z_transform_results[j] = prev_val + current_term
+
+        return self._z_transform_results[i]
+
+
+    def compute_approx_risk_until_dim_i(self, i):
+        """Compute the approximate risk at step T using the Z-transform result for dimension i."""
+        
+        m_0_to_i = copy.deepcopy(self.m0)
+        m_0_to_i[i+1:] = 0
+
+        irreducible_noise = self.model.sigma**2
+
+        v_t = np.zeros(self.model.dim)
+        risks = []
+        
+        for t in range(self.T):
+            # Le calcul du risque reste identique
+            bias_part = np.sum(self.model.Lambda_vals * m_0_to_i)
+            variance_part = np.sum(self.model.Lambda_vals * v_t)
+            risk = 0.5 * (bias_part + variance_part)
+            risks.append(risk)
+            
+            lr = self.schedule.get_base_lr()
+            
+            # Vecteur diagonal de base
+            diag_part = (1 - lr * self.model.Lambda_vals)**2 + (lr * self.model.Lambda_vals)**2
+            
+
+            approx_term_mt = (self.model.Lambda_vals**2) * m_0_to_i
+            approx_term_vt = (self.model.Lambda_vals**2) * v_t
+            
+            # Mise à jour totalement découplée (chaque dimension i vit sa vie de son côté)
+            m_0_to_i = diag_part * m_0_to_i + (lr**2) * approx_term_mt
+            v_t = diag_part * v_t + (lr**2) * approx_term_vt + (lr**2 * self.model.sigma**2) * self.model.Lambda_vals
+        
+        return np.array(risks)
+
+    def compute_approx_risk(self):
+        """Compute the approximate risk at step T using the Z-transform results for all dimensions."""
+        risks = []
+        for i in range(self.model.dim):
+            risk_i = self.compute_approx_risk_until_dim_i(i)
+            risks.append(risk_i[-1])  # On prend le risque à la dernière itération (T-1)
+        return np.array(risks)
+
+
+    def compute_all_z_transform_results(self):
+        """Compute the Z-transform results for all steps up to T-1."""
+        self.compute_z_transform_result(self.model.dim - 1)
+        return self._z_transform_results
+    
+
+
+    def compute_all_approx_vs_z_transform(self):
+        """Compute the Z-transform results and the approximate risks for all steps up to T-1."""
+        z_results = self.compute_all_z_transform_results()
+        approx_risks = self.compute_approx_risk()
+        return list(z_results.values()), approx_risks
+        
